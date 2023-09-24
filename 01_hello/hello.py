9dfbf8ad39b0641eb0f89b65597afca00dcc82aa
climatic7223 <tspray4@pm.me>
2023-09-24 09:57:29 -0400

    Second commit.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-"""
-Author: Tynan Spray <tfspray@gmail.com>
-Purpose: Say hello
-"""
+# Purpose: Say hello
 
 import argparse
+
 
 # ------------------------------------------------
 def get_args():
     """Get the command-line arguments"""
 
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(description="say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
     return parser.parse_args()
 
+
 # ------------------------------------------------
+
+
 def main():
-    """Make a jazz noise here"""
-
+    """Heavy lifting"""
     args = get_args()
-    print('Hello, ' + args.name + '!')
+    print("Hello, " + args.name + "!")
+
 
 # ------------------------------------------------
-if __name__ == '__main__':
-    """Where the program begins"""
-
+if __name__ == "__main__":
     main()
